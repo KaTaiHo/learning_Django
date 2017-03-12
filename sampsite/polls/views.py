fe68df419a7ee07a102d460de4c48a5854ff61f5
warrior5715 <kataihoo@gmail.com>
2017-03-12 00:41:19 -0600

    added questions to poll

--- sampsite/polls/views.py
+++ sampsite/polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question, Choice
+
 def index(request):
-    return HttpResponse("You're in the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at Question %s" % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s"
+    return HttpResponse(response % question_id)
+

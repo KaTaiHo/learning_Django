5b140224da7f0234783391727cb7267edfac9010
warrior5715 <kataihoo@gmail.com>
2017-03-13 00:45:54 -0500

    added Schedule class and fixed migration

--- scheduler/scheduler/courses/models.py
+++ scheduler/scheduler/courses/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Schedule(models.Model):
-    pass
+    year = models.IntegerField()
+    dept = models.CharField(max_length=4)
+    course_num = models.IntegerField()
+    unique = models.IntegerField()
+    title = models.CharField(max_length=15)
+    instructor = models.CharField(max_length=20)
+    days = models.CharField(max_length=6)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    building = models.CharField(max_length=5)
+    room = models.IntegerField()
+
+    def __str__(self):
+        return self.course_num + self.unique + self.instructor

7a6ca489c55387b7ec7f3baca627a987a2595898
warrior5715 <kataihoo@gmail.com>
2017-03-13 12:14:24 -0500

    added JSON return format

--- scheduler/scheduler/courses/views.py
+++ scheduler/scheduler/courses/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Schedule
+from .serializers import ScheduleSerializer
 # Create your views here.
+
+# list all schedules or create a new one
+class ScheduleList(APIView):
+
+    def get(self, request):
+        schedule = Schedule.objects.all()
+        serializer = ScheduleSerializer(schedule, many = True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass

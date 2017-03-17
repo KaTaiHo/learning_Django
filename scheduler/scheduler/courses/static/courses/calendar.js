4995a301b9cefe2941f45f2bba2a948edb046c82
warrior5715 <kataihoo@gmail.com>
2017-03-17 18:13:21 -0500

    testing modal

--- scheduler/scheduler/courses/static/courses/calendar.js
+++ scheduler/scheduler/courses/static/courses/calendar.js
@@ -34,10 +34,10 @@ function displayCourse() {
         "<div>" +
         "<p>" + "<strong>Title: </strong>" + courseArray[i]['title'] + "&emsp;<strong>Professor: </strong>" + courseArray[i]['instructor'] +
             "&emsp;<strong>Status: </strong>" + courseArray[i]['status'] + "&emsp;<strong>Course: </strong>" + courseArray[i]['dept'] + courseArray[i]['course_num']
-              + "&emsp;&emsp;<button type='button'>Add This Section</button>" + "</p>" +
+               + "</p>" +
         "<p>" + "<strong>Unique ID: </strong>" + courseArray[i]['id'] + "&emsp;<strong>Days: </strong>" + courseArray[i]['days'] +
             "&emsp;<strong>Time: </strong>" + courseArray[i]['start_time'] + " to " + courseArray[i]['end_time'] + "&emsp;<strong>Location: </strong>" + courseArray[i]['building'] +
-              "&emsp;" + courseArray[i]['room'] + "</p>" +
+              "&emsp;" + courseArray[i]['room'] + "&emsp;&emsp;<button type='button'>Add This Section</button>" + "</p>" +
         "</div><hr>"
         );
     }

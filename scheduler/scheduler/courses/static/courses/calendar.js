1d44acf8bd0eba36e23905312def286a708ae245
warrior5715 <kataihoo@gmail.com>
2017-03-18 13:39:16 -0500

    fixing db

--- scheduler/scheduler/courses/static/courses/calendar.js
+++ scheduler/scheduler/courses/static/courses/calendar.js
@@ -9,39 +9,42 @@ function populateCourseID() {
 }
 
 var sel = document.getElementById('course_id');
-var count = 0;
 
 $(document).ready(function() {
     $('#course_id').change(updateCourseList);
     $('#display-course').click(displayCourse);
     $('#myModal .close').click(closeModal);
     $(".modal-body").find(".button").click(function () {
-        var data = $(this).closest('div').attr("id");
-        data = JSON.parse(data);
-        alert(data);
-        var momentTime = getSpecificDate(new Date(), 4).toISOString().slice(0,11);
-        var startTime = "";
-        var endTime = "";
+        try {
+            var data = $(this).closest('div').attr("id");
+            data = JSON.parse(data);
+            alert(data);
+            var momentTime = getSpecificDate(new Date(), 4).toISOString().slice(0,11);
+            var startTime = "";
+            var endTime = "";
 
-        if (data.['start_time'].toString().length % 2 == 0) {
-            startTime = data['start_time'].toString().slice(0,2) + ":" + data['start_time'].toString().slice(2,4);
-        }
-        else {
-            startTime = data['start_time'].toString().slice(0,1) + ":" + data['start_time'].toString().slice(1,3);
-        }
+            if (data.['start_time'].toString().length % 2 == 0) {
+                startTime = data['start_time'].toString().slice(0,2) + ":" + data['start_time'].toString().slice(2,4);
+            }
+            else {
+                startTime = data['start_time'].toString().slice(0,1) + ":" + data['start_time'].toString().slice(1,3);
+            }
 
-        if (data.['end_time'].toString().length % 2 == 0) {
-            endTime = data['end_time'].toString().slice(0,2) + ":" + data['end_time'].toString().slice(2,4);
-        }
-        else {
-            endTime = data['end_time'].toString().slice(0,1) + ":" + data['end_time'].toString().slice(1,3);
-        }
+            if (data.['end_time'].toString().length % 2 == 0) {
+                endTime = data['end_time'].toString().slice(0,2) + ":" + data['end_time'].toString().slice(2,4);
+            }
+            else {
+                endTime = data['end_time'].toString().slice(0,1) + ":" + data['end_time'].toString().slice(1,3);
+            }
 
-        momentTime = startTime + endTime;
-        var m = $.fullCalendar.moment(momentTime);
-        var event={id:count , title: data['dept'] + data['course_num'], start:  m};
-        count++;
-        $('#calendar').fullCalendar( 'addEvent', event, true);
+            momentTime = startTime + endTime;
+            var m = $.fullCalendar.moment(momentTime);
+            var event={id:0 , title: data['dept'] + data['course_num'], start:  m};
+            $('#calendar').fullCalendar( 'addEvent', event, true);
+        }
+        catch(err){
+            alert(err.message);
+        }
     });
  });
 

97230e272da7742eb16540fbd02abe90d8621c51
warrior5715 <kataihoo@gmail.com>
2017-03-19 07:34:40 -0500

    fixing db

--- scheduler/scheduler/courses/static/courses/calendar.js
+++ scheduler/scheduler/courses/static/courses/calendar.js
@@ -31,7 +31,7 @@ function closeModal() {
 function getSpecificDate(d, num) {
   d = new Date(d);
   var day = d.getDay(),
-      diff = d.getDate() - day + (day == 0 ? -6:num); // adjust when day is sunday
+      diff = d.getDate() - day; // adjust when day is sunday  + (day == 0 ? -6:num)
   return new Date(d.setDate(diff));
 }
 
